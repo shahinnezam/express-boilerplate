38200129775e0eaf9983c02c7d3b94f2e5789712
Othman Djuliarso <othmandjuliarso@gmail.com>
2023-02-24 15:13:39 -0800

    edits authConfig

--- auth/authCheck.js
+++ auth/authCheck.js
@@ -1,6 +1,29 @@
-exports.authCheck = (req, res, next) => {
-    if (req.auth_token && isAuthenticated(req.auth_token))
-        next();
-    else
-        res.status(401).end();
-}
+// const jwt = require('jsonwebtoken');
+const authConfig = require('../auth/authConfig');
+const { JWT_SECRET } = require('../auth/authConfig');
+
+verifyToken = (req, res, next) => {
+  let token = req.session.token;
+
+  if (!token) {
+    return res.status(403).send({
+      message: 'No token provided!',
+    });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: 'Unauthorized',
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+const authJWT = {
+  verifyToken,
+};
+
+module.exports = authJWT;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json({data: 'list of users'});
+router.get('/', function (req, res, next) {
+  res.json({ data: 'list of users' });
 });
 
 module.exports = router;
